4cb0e003b4674ffcf7e538ab24700b14761dc383
Geraldina (laptop) <grushevskaya.d.v@yandex.ru>
2020-07-19 11:37:02 +0300

    Разный радиус захвата

--- processing_video/foreground_mask.py
+++ processing_video/foreground_mask.py
@@ -5,28 +5,34 @@ def compute_foreground_mask(place_mask, object_mask):
 
 def move_horizontally(place_mask, object_mask):
     height, width = place_mask.shape
+    radius = 10
     for h in range(height):
-        for w in  range(width - 1):
+        for w in  range(width - radius):
             if place_mask[h, w] == 1 and object_mask[h, w]:
-                if object_mask[h, w + 1] == 1:
-                    place_mask[h, w + 1] = 1
-        for w in range(width - 1, 0, -1):
+                for i in range(1, radius + 1):
+                    if object_mask[h, w + i] == 1:
+                        place_mask[h, w + i] = 1
+        for w in range(width - radius, 0, -1):
             if place_mask[h, w] == 1 and object_mask[h, w]:
-                if object_mask[h, w - 1] == 1:
-                    place_mask[h, w - 1] = 1
+                for i in range(1, radius + 1):
+                    if object_mask[h, w - i] == 1:
+                        place_mask[h, w - i] = 1
     return place_mask
 
 
 def move_vertically(place_mask, object_mask):
     height, width = place_mask.shape
+    radius = 10
     for w in range(width):
-        for h in range(height - 1):
+        for h in range(height - radius):
             if place_mask[h, w] == 1 and object_mask[h, w]:
-                if object_mask[h + 1, w] == 1:
-                    place_mask[h + 1, w] = 1
-        for h in range(height - 1, 0, -1):
+                for i in range(1, radius + 1):
+                    if object_mask[h + i, w] == 1:
+                        place_mask[h + i, w] = 1
+        for h in range(height - radius, 0, -1):
             if place_mask[h, w] == 1 and object_mask[h, w]:
-                if object_mask[h - 1, w] == 1:
-                    place_mask[h - 1, w] = 1
+                for i in range(1, radius + 1):
+                    if object_mask[h - i, w] == 1:
+                        place_mask[h - i, w] = 1
     return place_mask
 
