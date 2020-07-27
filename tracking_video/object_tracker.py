aca0aabe324336b70aeda256bd10a849df9d77be
Грушевская Дарья Владимировна <grushevskaya.d.v@yandex.ru>
2020-07-27 14:43:10 +0300

    Константы вынесены в отдельный файл

--- tracking_video/object_tracker.py
+++ tracking_video/object_tracker.py
@@ -8,6 +8,8 @@ import imutils
 import time
 import cv2
 
+from constants import *
+
 app = argparse.ArgumentParser()
 app.add_argument(
     "-v", "--video", type=str,
@@ -18,16 +20,6 @@ app.add_argument(
     help="OpenCV object tracker type"
 )
 args = vars(app.parse_args())
-
-OPENCV_OBJECT_TRACKERS = {
-    "csrt": cv2.TrackerCSRT_create,
-    "kcf": cv2.TrackerKCF_create,
-    "boosting": cv2.TrackerBoosting_create,
-    "mil": cv2.TrackerMIL_create,
-    "tld": cv2.TrackerTLD_create,
-    "medianflow": cv2.TrackerMedianFlow_create,
-    "mosse": cv2.TrackerMOSSE_create
-}
 
 tracker = OPENCV_OBJECT_TRACKERS[args["tracker"]]()
 
