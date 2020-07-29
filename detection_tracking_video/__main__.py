442b82ee6a83cc854ad2a7d13bc3047e31eea900
Грушевская Дарья Владимировна <grushevskaya.d.v@yandex.ru>
2020-07-29 09:50:58 +0300

    Инициализация модуля детекция и трекинг

---
+++ detection_tracking_video/__main__.py
@@ -0,0 +1,14 @@
+import argparse
+
+def parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--video", type=str, help="path to input video file")
+    parser.add_argument("-t", "--tracker", type=str, default="kcf", help="OpenCV object tracker type")
+    return parser.parse_args()
+
+
+args = vars(parse())
+if not args.get("video", False):
+    print("Нужно указать путь к видео. Параметр -v или --video.")
+else:
+    pass
