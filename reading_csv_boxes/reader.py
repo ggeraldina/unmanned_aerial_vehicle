75e021909e79a168f9d440cb96fc4d0a0b422e3d
Грушевская Дарья Владимировна <grushevskaya.d.v@yandex.ru>
2020-09-02 14:21:49 +0300

    bugfix: Переписан reader

--- reading_csv_boxes/reader.py
+++ reading_csv_boxes/reader.py
@@ -34,34 +34,33 @@ class Reader:
             framerate, (self._frame_width, self._frame_height)
         )
 
+
     def run(self):
-        with open(self._path_csv, newline='') as csv_file:
+        with open(self._path_csv, newline="") as csv_file:
             reader = csv.DictReader(csv_file)
-            for row in reader:
+            row = next(reader, None)
+            while(True):
                 if self._current_frame is None:
                     break
-                while int(row["frame"]) > self._amount_frame:
-                    if self._current_frame is None:
-                        break
-                    self._drow_count()
-                    cv2.namedWindow(DEFAULT_FRAME_WINDOW_NAME, cv2.WINDOW_NORMAL)
-                    cv2.imshow(DEFAULT_FRAME_WINDOW_NAME, self._current_frame)                    
-                    self._save_video()
-                    if self._check_commands() == EXIT_SUCCESS:
-                        break                        
-                    _, self._current_frame = self._capture.read()
-                    self._amount_frame += 1
-
-                if not row["logs"] == "":
-                    continue
-                cv2.rectangle(
-                    self._current_frame, (int(row["x"]), int(row["y"])),
-                    (int(row["x"]) + int(row["w"]), int(row["y"]) + int(row["h"])),
-                    (0, 0, 255),
-                    2
-                )                
+                if not row is None and int(row["frame"]) == self._amount_frame and not row["logs"] == "":
+                    row = reader.__next__()
+                if not row is None and int(row["frame"]) == self._amount_frame:
+                    cv2.rectangle(
+                        self._current_frame, (int(row["x"]), int(row["y"])),
+                        (int(row["x"]) + int(row["w"]), int(row["y"]) + int(row["h"])),
+                        (0, 0, 255),
+                        2
+                    )
+                    row = next(reader, None)
+                self._drow_count()
+                cv2.namedWindow(DEFAULT_FRAME_WINDOW_NAME, cv2.WINDOW_NORMAL)
+                cv2.imshow(DEFAULT_FRAME_WINDOW_NAME, self._current_frame)                    
+                self._save_video()
                 if self._check_commands() == EXIT_SUCCESS:
                     break                        
+                _, self._current_frame = self._capture.read()
+                self._amount_frame += 1
+
 
     def _drow_count(self):
         """ Нарисовать счетчик
